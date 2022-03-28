caa1feb7376d9db8c9efd74be52113580cb1ff69
Aaron <ajonaaronajon@yahoo.com>
2022-03-28 02:16:40 -0500

    added method to add tasks to tasks array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      tasks: []
+    }
+
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask(e){
+    e.preventDefault();
+    const task = document.getElementById('add-task').value;
+
+    //updater function to add new task to tasks array
+    this.setState(prevState => ({ tasks: [...prevState.tasks, task] }));
+
   }
 
   render(){
@@ -12,7 +27,7 @@ class App extends Component {
         <div>
           <label htmlFor="add-task">Add Task: </label>
           <input type='text' id='add-task' name='add-task'/>
-          <input type='submit' id='submit'/>
+          <input onClick = { this.addTask }type='submit' id='submit'/>
         </div>
 
       </form>
